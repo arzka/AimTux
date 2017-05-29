aca96be7060b8aeacf2aec4ab0bf436b2906ffa0
arzka <arzkamail@luukku.com>
2017-05-29 22:36:23 +0300

    Update hvhtab.cpp

--- src/ATGUI/Tabs/hvhtab.cpp
+++ src/ATGUI/Tabs/hvhtab.cpp
@@ -3,13 +3,17 @@
 void HvH::RenderTab()
 {
 	const char* yTypes[] = {
-			"SLOW SPIN", "FAST SPIN", "SPIN_RANDOM", "JITTER", "CASUALJITTER", "RANDOMBACKJITTER", "BACKJITTER", "SIDE", "BACKWARDS", "FORWARDS", "LEFT", "RIGHT", "STATIC", "STATIC JITTER", "STATIC SMALL JITTER", "LOWERBODY", "LBYJITTER",  // safe
-			"LISP", "LISP SIDE", "LISP JITTER", "ANGEL BACKWARDS", "ANGEL INVERSE", "ANGEL SPIN" // untrusted
+			"SLOW SPIN", "FAST SPIN", "RANDOM SPIN", "LBY SPIN", "JITTER", "RANDOM BACKJITTER", "BACKJITTER", "TURBO JITTER", "SIDE", "BACKWARDS", "FORWARDS", "LEFT", "RIGHT", "STATIC", "STATIC JITTER", "STATIC SMALL JITTER", "LUA", "LUA2", "CASUALAA",// safe
+			"LISP", "LISP SIDE", "LISP JITTER", "ANGEL BACKWARDS", "ANGEL INVERSE", "ANGEL SPIN", "LOWERBODY", "LBYONGROUND", "LUA UNCLAMPED", "LUA UNCLAMPED2", // untrusted
 	};
 
 	const char* xTypes[] = {
-			"UP", "DOWN", "DANCE", "FRONT", // safe
-			"FAKE UP", "FAKE DOWN", "LISP DOWN", "ANGEL DOWN", "ANGEL UP" // untrusted
+			"UP", "DOWN", "DANCE", "FRONT", "FAKE PITCH", "LUA", // safe
+			"FAKE UP", "FAKE DOWN", "LISP DOWN", "ANGEL DOWN", "ANGEL UP", "LUA UNCLAMPED" // untrusted
+	};
+
+	const char* resolverModes[] = {
+			"OFF", "FORCE", "DELTA", "STEADY", "TICKMODULO", "POSEPARAM", "ALL"
 	};
 
 	ImGui::Columns(2, NULL, true);
@@ -126,12 +130,71 @@ void HvH::RenderTab()
 		{
 			ImGui::Text("Resolver");
 			ImGui::Separator();
-			ImGui::Checkbox("Resolve All", &Settings::Resolver::resolveAll);
-			SetTooltip("Resolves all players on the server");
+			ImGui::Combo("##RESOLVERMODE", (int*)& Settings::Resolver::mode, resolverModes, IM_ARRAYSIZE(resolverModes));
+			ImGui::SliderFloat("##RESOLVERTICKS", &Settings::Resolver::ticks, 0, 16, "Ticks: %0.f");
+			ImGui::SliderFloat("##RESOLVERMODULO", &Settings::Resolver::modulo, 0, 16, "Modulo: %0.f");
 			ImGui::Separator();
 			ImGui::Text("Movement");
 			ImGui::Checkbox("Auto Crouch", &Settings::Aimbot::AutoCrouch::enabled);
 			SetTooltip("Auto crouch when an enemy is in sight");
+
+			ImGui::Separator();
+			ImGui::Checkbox("Lua Debug Mode", &Settings::AntiAim::Lua::debugMode);
+			SetTooltip("Turns ON/OFF Lua Syntax/Runtime error checking.\n *Errors found in CSGO Console\nCan be turned off once Script is done for more speed.");
+			if( Settings::AntiAim::Pitch::type == AntiAimType_X::LUA1 || Settings::AntiAim::Pitch::type == AntiAimType_X ::LUA_UNCLAMPED )
+			{
+				ImGui::Text("Lua AntiAim Editor -- X Axis");
+				ImGui::InputTextMultiline("##LUAX", Settings::AntiAim::Lua::scriptX, sizeof(Settings::AntiAim::Lua::scriptX));
+			}
+			ImGui::Separator();
+
+			if( ((Settings::AntiAim::Yaw::type == Settings::AntiAim::Yaw::typeFake) && // if they are equal to each other and a LUA type
+						(Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA1 ||
+						Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA2  ||
+						Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA_UNCLAMPED ||
+						Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA_UNCLAMPED2))
+				|| // OR
+					( (Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA1 && Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::LUA_UNCLAMPED)// Any LUA types that use the same underlying script.
+					 || (Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA2 && Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::LUA_UNCLAMPED2)
+					 || (Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA_UNCLAMPED && Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::LUA1)
+					 || (Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA_UNCLAMPED2 && Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::LUA2)
+					)
+				)
+		    {
+				ImGui::Text("Lua AntiAim Editor -- Y Axis(BOTH)");
+				if( Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA1 || Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA_UNCLAMPED )
+				{
+					ImGui::InputTextMultiline("##LUAY", Settings::AntiAim::Lua::scriptY, sizeof(Settings::AntiAim::Lua::scriptY));
+				}
+				else
+				{
+					ImGui::InputTextMultiline("##LUAY2", Settings::AntiAim::Lua::scriptY2, sizeof(Settings::AntiAim::Lua::scriptY2));
+				}
+			}
+			else
+			{
+				if ( Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA1 || Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA_UNCLAMPED )
+				{
+					ImGui::Text("Lua AntiAim Editor -- Y Axis(ACTUAL)");
+					ImGui::InputTextMultiline("##LUAY", Settings::AntiAim::Lua::scriptY, sizeof(Settings::AntiAim::Lua::scriptY));
+				}
+				else if ( Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA2 || Settings::AntiAim::Yaw::type == AntiAimType_Y::LUA_UNCLAMPED2 )
+				{
+					ImGui::Text("Lua AntiAim Editor -- Y2 Axis(ACTUAL)");
+					ImGui::InputTextMultiline("##LUAY2", Settings::AntiAim::Lua::scriptY2, sizeof(Settings::AntiAim::Lua::scriptY2));
+				}
+				ImGui::Separator();
+				if( Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::LUA1 || Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::LUA_UNCLAMPED )
+				{
+					ImGui::Text("Lua AntiAim Editor -- Y Axis(FAKE)");
+					ImGui::InputTextMultiline("##LUAY", Settings::AntiAim::Lua::scriptY, sizeof(Settings::AntiAim::Lua::scriptY));
+				}
+				else if( Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::LUA2 || Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::LUA_UNCLAMPED2 )
+				{
+					ImGui::Text("Lua AntiAim Editor -- Y2 Axis (FAKE)");
+					ImGui::InputTextMultiline("##LUAY2", Settings::AntiAim::Lua::scriptY2, sizeof(Settings::AntiAim::Lua::scriptY2));
+				}
+			}
 			ImGui::EndChild();
 		}
 	}
